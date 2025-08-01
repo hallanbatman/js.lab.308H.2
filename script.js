cf78efc4948ef43a95467c04e81e2183aed14fd3
Hallan Batman <batman.hallan@gmail.com>
2025-08-01 09:02:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ let radius = radiusContainedArea;
 // The formula for calculating the area of a circle is PI multiplied by the radius, squared:
 const PI = 3.1415;
 const area = PI * radius * radius;
-console.log (`Initial contained area: ${area} m2.`);
+console.log(`Initial contained area: ${area} m2.`);
+
+const area80 = area * 0.8
+console.log(`80% of contained area: ${area80} m2.`);
+
+const area50 = area * 0.5
+console.log(`50% of contained area: ${area50} m2.`);
 
 // Each plant requires a minimum space of 0.8 square meters.
 let plantSize = 0.8;//square meters
@@ -14,27 +20,65 @@ let plantSize = 0.8;//square meters
 let numPlants = 20;//plants
 
 // Initial area size reuqired
-let areaOccupied = numPlants*plantSize
-console.log (`Initial occupied area size: ${areaOccupied} m2.`)
+let areaOccupied = (numPlants * 1) * plantSize
+console.log(`Initial occupied area size: ${areaOccupied} m2.`)
+
+// Check for week 1
+let week = 1;
+areaOccupied = numPlants * 2; // week 1, plants double
+console.log(areaOccupied)
+
+console.log(`week ` + week + `:`)
+console.log(`plants ` + plants)
+
+
+if (plants > area80) {
+    console.log(`Action: Prune`);
+} else if (plants >= area50) {
+    console.log(`  Action: Monitor`);
+} else {
+    console.log(`Plant more`);
+}
+
+// Check for week 2
+week = 2;
+plants = (numPlants * 2) * 2; // week 2, double again
+
+console.log(`week ` + week + `:`)
+console.log(`plants ` + plants)
+
+if (plants > plantSize * area) {
+    console.log(`Action: Prune`);
+} else if (plants >= 0.5 * area) {
+    console.log(`  Action: Monitor`);
+} else {
+    console.log(`Plant more`);
+}
+
+
+
+
+//What's 50% of area?
+//What's 880% of area?
 
 // The plants double in number every week.
-const growthRate = 2*plantSize;//per week
-const week = 1
-
-//week 0 = 20 plants
-//week 1 = 40 plants
-//week 2 = 80 plants
-//week 3 = 160 plants
+const growthRate = 2 * numPlants;//per week
 
 // Objectives
 
 // Predict the plant growth after a specific number of weeks.
 // include the results for 1, 2, and 3 weeks of growth as inputs.
-console.log(`Total plant size growth in square m2:`)
-console.log(`0 Week = ${numPlants*plantSize} m2`)
-console.log(`1 Week = ${(numPlants*2)*plantSize} m2`)
-console.log(`2 Week = ${(numPlants*3)*plantSize} m2`)
-console.log(`3 Week = ${(numPlants*4)*plantSize} m2`)
+//console.log(`Total plant size growth in square m2:`)
+
+//week 0 = 20 plants * 0.8 m2 = 16 m2
+
+//week 1 = 40 plants * 0.8 m2 = 32 m2
+
+//week 2 = 80 plants * 0.8 m2 = 64m2
+
+//week 3 = 160 plants * 0.8 m2 = 128m2
+
+
 
 
 // Implement control flow to make decisions on whether the plants should be:
